698ff9291b1d165e111f56160c90cbf444ebf8e1
Brice Ernst <brice.ernst1@gmail.com>
2022-03-22 11:30:07 -0500

    student tests

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,2 +1,31 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class StudentTest {
+    Student student = new Student(1, "Bob");
+
+    @Test
+    public void testIfIdIsSet(){
+        assertEquals(1, student.getId(), 0);
+    }
+
+    @Test
+    public void testIfNameIsSet(){
+        assertEquals("Bob", student.getName());
+    }
+
+    @Test
+    public void testIfGradeIsSet(){
+        student.addGrade(100);
+        assertEquals((Integer) 100, student.getGrades().get(0));
+    }
+
+    @Test
+    public void testIfAverageIsSet(){
+        student.addGrade(100);
+        student.addGrade(97);
+        student.addGrade(97);
+        assertEquals(98, student.getGradeAverage(),0);
+    }
 }
